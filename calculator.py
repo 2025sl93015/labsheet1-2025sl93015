92ea4a39a4746c4bdc717d5078418038c930cfb3
2025sl93015 <2025sl93015@wilp.bits-pilani.ac.in>
2026-04-07 17:13:26 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,14 +2,9 @@ def add(a, b):
 	return a+b
 def multiply(a, b):
 	return a*b
-<<<<<<< HEAD
-
 def subtract(a, b):
 	return a-b
-=======
 def divide(a, b):
 	if b==0:
 		return None
 	return a/b
-
->>>>>>> 312ce0b (Add divide functionality)
